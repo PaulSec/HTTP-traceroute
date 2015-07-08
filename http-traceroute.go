8390499dd2aa9e6124c01ee44cfb9bdb24b028df
PaulSec <aos.paul@gmail.com>
2015-07-08 09:45:24 +0100

    converted indentation to spaces

--- http-traceroute.go
+++ http-traceroute.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"net/http"
-	"net/http/cookiejar"
+    "fmt"
+    "os"
+    "net/http"
+    "net/http/cookiejar"
 )
 
 type LogRedirects struct {
-	    Transport http.RoundTripper
+        Transport http.RoundTripper
 }
 
 func (l LogRedirects) RoundTrip(req *http.Request) (resp *http.Response, err error) {
@@ -21,61 +21,61 @@ func (l LogRedirects) RoundTrip(req *http.Request) (resp *http.Response, err err
         return
     }
     switch resp.StatusCode {
-    	case http.StatusMovedPermanently, http.StatusFound, http.StatusSeeOther, http.StatusTemporaryRedirect:
-        	fmt.Println("#### Redirected ####\n")
-        	fmt.Println("Request for:", req.URL)
-        	fmt.Println("Redirected to:", resp.Header.Get("location"))
-        	fmt.Println("Status code:", resp.StatusCode)
+        case http.StatusMovedPermanently, http.StatusFound, http.StatusSeeOther, http.StatusTemporaryRedirect:
+            fmt.Println("#### Redirected ####\n")
+            fmt.Println("Request for:", req.URL)
+            fmt.Println("Redirected to:", resp.Header.Get("location"))
+            fmt.Println("Status code:", resp.StatusCode)
 
-        	fmt.Println("\n\n#### Headers ####\n")
+            fmt.Println("\n\n#### Headers ####\n")
 
-		    for k, v := range resp.Header {
-		        fmt.Println(k, ": ", v[0])
-		    }
+            for k, v := range resp.Header {
+                fmt.Println(k, ": ", v[0])
+            }
 
-			fmt.Println("\n\n#### Cookies ####\n")
-			for _, cookie := range resp.Header[http.CanonicalHeaderKey("Set-Cookie")] {
-				fmt.Println(cookie)
-			}
+            fmt.Println("\n\n#### Cookies ####\n")
+            for _, cookie := range resp.Header[http.CanonicalHeaderKey("Set-Cookie")] {
+                fmt.Println(cookie)
+            }
 
-			if resp.Header[http.CanonicalHeaderKey("Set-Cookie")] == nil {
-				fmt.Println("No cookies created.")
-			}
+            if resp.Header[http.CanonicalHeaderKey("Set-Cookie")] == nil {
+                fmt.Println("No cookies created.")
+            }
 
-		    fmt.Println("\n")
+            fmt.Println("\n")
     }
     return
 }
 
 func main() {
-	// create a cookiejar
-	cookieJar, _ := cookiejar.New(nil)
+    // create a cookiejar
+    cookieJar, _ := cookiejar.New(nil)
 
-	// create a client
-	client := http.Client{Jar: cookieJar, Transport: LogRedirects{}}
+    // create a client
+    client := http.Client{Jar: cookieJar, Transport: LogRedirects{}}
 
-	fmt.Println("Initial request to:", os.Args[1], "\n")
+    fmt.Println("Initial request to:", os.Args[1], "\n")
 
-	// do the request
-	resp, err := client.Get(os.Args[1])
-	if err != nil {
-		fmt.Println("[!] Error while accessing the host.")
-		return
-	}
-	fmt.Println("Final URL is", resp.Request.URL, " with status code: ", resp.StatusCode)
+    // do the request
+    resp, err := client.Get(os.Args[1])
+    if err != nil {
+        fmt.Println("[!] Error while accessing the host.")
+        return
+    }
+    fmt.Println("Final URL is", resp.Request.URL, " with status code: ", resp.StatusCode)
 
-	// display information for the last request
-	fmt.Println("\n\n#### Headers ####\n")
+    // display information for the last request
+    fmt.Println("\n\n#### Headers ####\n")
     for k, v := range resp.Header {
         fmt.Println(k, ": ", v[0])
     }
 
-	fmt.Println("\n\n#### Cookies ####\n")
-	for _, cookie := range resp.Header[http.CanonicalHeaderKey("Set-Cookie")] {
-		fmt.Println(cookie)
-	}
+    fmt.Println("\n\n#### Cookies ####\n")
+    for _, cookie := range resp.Header[http.CanonicalHeaderKey("Set-Cookie")] {
+        fmt.Println(cookie)
+    }
 
-	if resp.Header[http.CanonicalHeaderKey("Set-Cookie")] == nil {
-		fmt.Println("No cookies created.")
-	}
+    if resp.Header[http.CanonicalHeaderKey("Set-Cookie")] == nil {
+        fmt.Println("No cookies created.")
+    }
 }
